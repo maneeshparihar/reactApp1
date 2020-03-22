3f3c6809bff412c15e164319de665f809ad63246
maneeshparihar <maneeshparihar@gmail.com>
2020-03-22 22:49:04 +0530

    showing next move

--- src/index.js
+++ src/index.js
@@ -2,29 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state={
-            value: null,
-        }
-    };
 
-    render() {
-      return (
-        <button className="square" 
-        onClick={()=>{ this.props.onClick(); }}>
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+  function Square(props){
+    return (
+      <button className="square" 
+      onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  } 
+  
   
   class Board extends React.Component {
     constructor(props){
         super(props);
         this.state={
             squares: Array(9).fill(null),
+            xIsNext: true,
         }
     }
     renderSquare(i) {
@@ -34,7 +28,7 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: '+ (this.state.xIsNext?'X':'O');
   
       return (
         <div>
@@ -60,8 +54,11 @@ class Square extends React.Component {
 
     handleClick(i){
       const squares = this.state.squares.slice();
-      squares[i]="X";
-      this.setState({squares: squares})
+      squares[i]= this.state.xIsNext?'X':'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      })
     }
   }
   
